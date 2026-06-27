c8fa0ebb50fe9be24f7b8f5a0439c1fda90de6c9
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-27 16:03:22 +0530

    Update dashboard.js

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -3,12 +3,13 @@
  * Project Phoenix
  * Product : Myntra Analytics
  * Module  : Dashboard Page
- * Version : V1.2
+ * Version : V1.3
  * =====================================================
  */
 
 import { renderKPIRow } from "../../components/dashboard/kpiRow/kpiRow.js";
 import { renderRevenueTrend } from "../../components/dashboard/revenueTrend/revenueTrend.js";
+import { renderTopStyles } from "../../components/dashboard/topStyles/topStyles.js";
 
 let dashboardContainer = null;
 
@@ -65,16 +66,30 @@ export async function refreshDashboard(){
     await renderRevenueTrend(revenueSection);
 
     // ======================================
+    // Top 20 Styles
+    // ======================================
+
+    const topStyleSection = createSection();
+
+    dashboardContainer.appendChild(topStyleSection);
+
+    await renderTopStyles(topStyleSection);
+
+    // ======================================
     // Future Widgets
     // ======================================
+
+    // const poTypeSection = createSection();
+    // dashboardContainer.appendChild(poTypeSection);
+    // await renderPOTypePerformance(poTypeSection);
 
     // const brandSection = createSection();
     // dashboardContainer.appendChild(brandSection);
     // await renderBrandPerformance(brandSection);
 
-    // const categorySection = createSection();
-    // dashboardContainer.appendChild(categorySection);
-    // await renderCategoryPerformance(categorySection);
+    // const dayOnDaySection = createSection();
+    // dashboardContainer.appendChild(dayOnDaySection);
+    // await renderDayOnDaySales(dayOnDaySection);
 
 }
 
