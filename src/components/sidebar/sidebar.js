71e6ff7593d3f7d5c0824b6626bb266473559fe4
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-27 08:46:31 +0530

    Update sidebar.js

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,36 +1,30 @@
 /**
  * =====================================================
  * Project Phoenix
- * Sidebar
+ * Product : Myntra Analytics
+ * Module  : Sidebar
+ * Author  : Saurabh Mondal & Magical CTO
  * =====================================================
  */
 
 import { loadComponent } from "../../utils/loadComponent.js";
-
 import { Assets } from "../../config/assets.js";
-
 import { Version } from "../../config/version.js";
+import { Navigation } from "../../config/navigation.js";
 
-export async function renderSidebar(target){
+export async function renderSidebar(target) {
 
     await loadComponent(
-
         target,
-
         "src/components/sidebar/sidebar.html",
-
         {
-
-            logo:Assets.logo,
-
-            appName:Version.app,
-
-            tagLine:"Fashion Data Intelligence",
-
-            version:Version.release+" • "+Version.version
-
+            logo: Assets.logo,
+            appName: Version.app,
+            tagLine: "Fashion Data Intelligence",
+            version: `${Version.release} • ${Version.version}`
         }
-
     );
+
+    buildMenu();
 
 }
