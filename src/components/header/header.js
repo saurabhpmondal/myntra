b303f4123e77cbdd70088147fde2a8265880d130
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-27 10:50:43 +0530

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,25 @@
 import { loadComponent } from "../../utils/loadComponent.js";
+import { loadStyle } from "../../utils/loadStyle.js";
 import { renderIcons } from "../../utils/icon.js";
 
 export async function renderHeader(target, title = "Dashboard") {
 
+    await loadStyle(
+        "src/components/header/header.css"
+    );
+
     await loadComponent(
+
         target,
+
         "src/components/header/header.html",
+
         {
+
             title
+
         }
+
     );
 
     document.getElementById("last-refresh").textContent =
