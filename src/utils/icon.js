30a672fa6778d0aced1bdb9ef2077ee1bc082cd7
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-27 08:51:01 +0530

    Update icon.js

--- src/utils/icon.js
+++ src/utils/icon.js
@@ -1,1 +1,18 @@
-test
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : Icon Engine
+ * Author  : Saurabh Mondal & Magical CTO
+ * =====================================================
+ */
+
+export function renderIcons() {
+
+    if (window.lucide) {
+
+        window.lucide.createIcons();
+
+    }
+
+}
