0f40f38d2149617f18fac43d7f888de32fdc6f79
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-27 17:22:27 +0530

    Update menu.js

--- src/components/sidebar/menu.js
+++ src/components/sidebar/menu.js
@@ -1,26 +1,29 @@
 /**
  * =====================================================
  * Project Phoenix
- * Sidebar Menu
+ * Product : Myntra Analytics
+ * Module  : Sidebar Menu
+ * Version : V2.0
  * =====================================================
  */
 
 import { Navigation } from "../../config/navigation.js";
 import { renderIcons } from "../../utils/icon.js";
+import { openPage } from "../../app/pageManager.js";
 
-export function buildMenu() {
+export function buildMenu(){
 
     const menu = document.querySelector(".sidebar-menu");
 
     menu.innerHTML = "";
 
-    Navigation.forEach((page, index) => {
+    Navigation.forEach((page,index)=>{
 
         const item = document.createElement("button");
 
         item.className = "sidebar-item";
 
-        if (index === 0) {
+        if(index===0){
 
             item.classList.add("active");
 
@@ -35,6 +38,26 @@ export function buildMenu() {
             <span>${page.title}</span>
 
         `;
+
+        item.onclick = async ()=>{
+
+            // Remove Active
+
+            document
+                .querySelectorAll(".sidebar-item")
+                .forEach(button=>{
+
+                    button.classList.remove("active");
+
+                });
+
+            item.classList.add("active");
+
+            // Open Page
+
+            await openPage(page.id);
+
+        };
 
         menu.appendChild(item);
 
