6dbb293430b21e121b840717fcdd104bd2287738
saurabhpmondal <saurabhpmondal@gmail.com>
2026-06-27 08:08:19 +0530

    Update assets.js

--- src/config/assets.js
+++ src/config/assets.js
@@ -1,1 +1,14 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * =====================================================
+ */
 
+export const Assets = {
+
+    logo: "assets/logo/logo.jpeg",
+
+    favicon: "assets/favicon/favicon.ico"
+
+};
