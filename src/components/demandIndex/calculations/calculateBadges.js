d74562cf3f4fce34877de6216e86cf228960a2ca
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 00:27:09 +0530

    Update calculateBadges.js

--- src/components/demandIndex/calculations/calculateBadges.js
+++ src/components/demandIndex/calculations/calculateBadges.js
@@ -2,133 +2,39 @@
  * =====================================================
  * Project Phoenix
  * Product : Myntra Analytics
- * Module  : Calculate Badges
- * Version : V1.0
+ * Module  : Badge Engine
+ * Version : V2.0
  * =====================================================
  */
 
-import { BADGE_RULES } from "../config/badgeRules.js";
+import { marketLeader } from "./badges/marketLeader.js";
 
-export function calculateBadges(
+import { brandLeader } from "./badges/brandLeader.js";
 
-    row
+import { top10 } from "./badges/top10.js";
 
-){
+import { top50 } from "./badges/top50.js";
 
-    const badges=[];
+import { eliteStyle } from "./badges/eliteStyle.js";
 
-    if(
+import { coreStyle } from "./badges/coreStyle.js";
 
-        row.overallRank===
+export function calculateBadges(row){
 
-        BADGE_RULES.MARKET_LEADER
+    return[
 
-    ){
+        ...marketLeader(row),
 
-        badges.push(
+        ...brandLeader(row),
 
-            "👑 Market Leader"
+        ...top10(row),
 
-        );
+        ...top50(row),
 
-    }
+        ...eliteStyle(row),
 
-    if(
+        ...coreStyle(row)
 
-        row.brandRank===
-
-        BADGE_RULES.BRAND_LEADER
-
-    ){
-
-        badges.push(
-
-            "🥇 Brand Leader"
-
-        );
-
-    }
-
-    if(
-
-        row.overallRank<=
-
-        BADGE_RULES.TOP_10
-
-    ){
-
-        badges.push(
-
-            "🔥 Top 10"
-
-        );
-
-    }
-
-    if(
-
-        row.overallRank<=
-
-        BADGE_RULES.TOP_50
-
-    ){
-
-        badges.push(
-
-            "⭐ Top 50"
-
-        );
-
-    }
-
-    if(
-
-        row.overallDW>=
-
-        BADGE_RULES.ELITE_DW
-
-    ){
-
-        badges.push(
-
-            "💎 Elite Style"
-
-        );
-
-    }
-
-    if(
-
-        row.cumulativeDW<=
-
-        BADGE_RULES.CORE_STYLE
-
-    ){
-
-        badges.push(
-
-            "🎯 Core Style"
-
-        );
-
-    }
-
-    if(
-
-        Number(row.rating||0)>=
-
-        BADGE_RULES.CUSTOMER_RATING
-
-    ){
-
-        badges.push(
-
-            "❤️ Customer Favourite"
-
-        );
-
-    }
-
-    return badges;
+    ];
 
 }
