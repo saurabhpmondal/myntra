31c04dcaf0382dfe57ec0ade8e011357bf574922
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 20:55:33 +0530

    Update losingMomentum.js

--- src/pages/demandIndex/calculations/badges/losingMomentum.js
+++ src/pages/demandIndex/calculations/badges/losingMomentum.js
@@ -3,7 +3,7 @@
  * Project Phoenix
  * Product : Myntra Analytics
  * Module  : Losing Momentum Badge
- * Version : V1.0
+ * Version : V2.0
  * =====================================================
  */
 
@@ -12,6 +12,28 @@ export function losingMomentum(
     row
 
 ){
+
+    if(
+
+        row.rankMovement==="DOWN"
+
+        &&
+
+        Math.abs(
+
+            row.rankChange
+
+        )>=20
+
+    ){
+
+        return [
+
+            "📉 Losing Momentum"
+
+        ];
+
+    }
 
     return [];
 
