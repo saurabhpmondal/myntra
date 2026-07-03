f9d46ab46347850ac203f4b11f3c354d0697628e
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 19:17:14 +0530

    Create stable.js

---
+++ src/pages/demandIndex/calculations/badges/stable.js
@@ -0,0 +1,18 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : Stable Badge
+ * Version : V1.0
+ * =====================================================
+ */
+
+export function stable(
+
+    row
+
+){
+
+    return [];
+
+}
