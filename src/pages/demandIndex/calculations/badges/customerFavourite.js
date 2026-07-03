5192e1fb2e710861f7c1a64522c18eaf1ce0db01
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 20:56:08 +0530

    Update customerFavourite.js

--- src/pages/demandIndex/calculations/badges/customerFavourite.js
+++ src/pages/demandIndex/calculations/badges/customerFavourite.js
@@ -3,7 +3,7 @@
  * Project Phoenix
  * Product : Myntra Analytics
  * Module  : Customer Favourite Badge
- * Version : V1.0
+ * Version : V2.0
  * =====================================================
  */
 
@@ -12,6 +12,24 @@ export function customerFavourite(
     row
 
 ){
+
+    if(
+
+        Number(
+
+            row.rating || 0
+
+        )>=4.0
+
+    ){
+
+        return [
+
+            "❤️ Customer Favourite"
+
+        ];
+
+    }
 
     return [];
 
