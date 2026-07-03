4731acfb7f64e16cca29aa029eef4840cc55f415
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 19:18:17 +0530

    Create nationwideHero.js

---
+++ src/pages/demandIndex/calculations/badges/nationwideHero.js
@@ -0,0 +1,18 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : Nationwide Hero Badge
+ * Version : V1.0
+ * =====================================================
+ */
+
+export function nationwideHero(
+
+    row
+
+){
+
+    return [];
+
+}
