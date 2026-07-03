6333691da0d624d6d05f69bcd2309cc6bf6376db
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 19:16:49 +0530

    Create losingMomentum.js

---
+++ src/pages/demandIndex/calculations/badges/losingMomentum.js
@@ -0,0 +1,18 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : Losing Momentum Badge
+ * Version : V1.0
+ * =====================================================
+ */
+
+export function losingMomentum(
+
+    row
+
+){
+
+    return [];
+
+}
