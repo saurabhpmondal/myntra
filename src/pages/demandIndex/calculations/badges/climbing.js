0087c888562711d2bf5e8c9123776a68bdad3f71
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 19:16:26 +0530

    Create climbing.js

---
+++ src/pages/demandIndex/calculations/badges/climbing.js
@@ -0,0 +1,18 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : Climbing Badge
+ * Version : V1.0
+ * =====================================================
+ */
+
+export function climbing(
+
+    row
+
+){
+
+    return [];
+
+}
