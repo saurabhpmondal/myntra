5a67262e95c6a30567891b52a0c4cbcfdf28263b
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-03 19:17:36 +0530

    Create customerFavourite.js

---
+++ src/pages/demandIndex/calculations/badges/customerFavourite.js
@@ -0,0 +1,18 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : Customer Favourite Badge
+ * Version : V1.0
+ * =====================================================
+ */
+
+export function customerFavourite(
+
+    row
+
+){
+
+    return [];
+
+}
