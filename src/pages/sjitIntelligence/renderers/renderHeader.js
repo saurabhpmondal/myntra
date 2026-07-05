fab7d6ab90066004eab632c33fe28a3ebaac5cc1
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-06 00:37:28 +0530

    Create renderers/renderHeader.js

---
+++ src/pages/sjitIntelligence/renderers/renderHeader.js
@@ -0,0 +1,44 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : SJIT Header
+ * Version : V1.0
+ * =====================================================
+ */
+
+export async function renderHeader(
+
+    target
+
+){
+
+    target.innerHTML=`
+
+<div class="dashboard-card">
+
+    <div class="card-header">
+
+        <div>
+
+            <h2>
+
+                SJIT Intelligence
+
+            </h2>
+
+            <p>
+
+                Stock Performance & Regional Sales Analysis
+
+            </p>
+
+        </div>
+
+    </div>
+
+</div>
+
+`;
+
+}
