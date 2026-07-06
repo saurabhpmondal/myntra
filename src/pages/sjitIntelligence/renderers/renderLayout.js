8ed3b808f82cbd9dac57b8c682644c081c166356
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-06 21:56:59 +0530

    Update renderLayout.js

--- src/pages/sjitIntelligence/renderers/renderLayout.js
+++ src/pages/sjitIntelligence/renderers/renderLayout.js
@@ -3,9 +3,12 @@
  * Project Phoenix
  * Product : Myntra Analytics
  * Module  : SJIT Layout
- * Version : V1.1
+ * Version : V2.0
  * =====================================================
  */
+
+import { createComponent }
+from "../../../utils/createComponent.js";
 
 export async function renderLayout(
 
@@ -13,62 +16,18 @@ export async function renderLayout(
 
 ){
 
-    target.innerHTML=`
+    await createComponent({
 
-<div class="dashboard-section">
+        target,
 
-    <div id="sjitHeader"></div>
+        html:
 
-</div>
+            "src/pages/sjitIntelligence/renderers/renderLayout.html",
 
-<div class="dashboard-section">
+        css:
 
-    <div id="sjitKPIs"></div>
+            "src/pages/sjitIntelligence/sjitIntelligence.css"
 
-</div>
-
-<div class="dashboard-section">
-
-    <div id="sjitInsights"></div>
-
-</div>
-
-<div
-    class="dashboard-section"
-    style="
-        display:grid;
-        grid-template-columns:2fr 1fr;
-        gap:20px;
-    "
->
-
-    <div id="sjitMap"></div>
-
-    <div id="sjitPie"></div>
-
-</div>
-
-<div class="dashboard-section">
-
-    <div id="sjitFCReport"></div>
-
-</div>
-
-<div
-    class="dashboard-section"
-    style="
-        display:grid;
-        grid-template-columns:1fr 1fr;
-        gap:20px;
-    "
->
-
-    <div id="sjitStateReport"></div>
-
-    <div id="sjitRegionReport"></div>
-
-</div>
-
-`;
+    });
 
 }
