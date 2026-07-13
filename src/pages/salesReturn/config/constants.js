8eb292881ca745b80784748c1770152d16ab6d49
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-13 06:42:07 +0530

    Create constants.js

---
+++ src/pages/salesReturn/config/constants.js
@@ -0,0 +1,78 @@
+/**
+ * =====================================================
+ * Project Phoenix
+ * Product : Myntra Analytics
+ * Module  : Constants
+ * Version : V1.0
+ * =====================================================
+ */
+
+export const MODULE={
+
+    NAME:
+
+        "Sales & Return Intelligence",
+
+    VERSION:
+
+        "V1.0"
+
+};
+
+export const FILTERS={
+
+    DEFAULT_MONTH:[],
+
+    DEFAULT_BRAND:[],
+
+    DEFAULT_PO_TYPE:[],
+
+    DEFAULT_RISK:[],
+
+    DEFAULT_SEARCH:""
+
+};
+
+export const TABLE={
+
+    PAGE_SIZE:50
+
+};
+
+export const SEARCH={
+
+    DEBOUNCE:300
+
+};
+
+export const EXPORT={
+
+    FILE_NAME:
+
+        "Sales_Return_Intelligence"
+
+};
+
+export const REPORTS={
+
+    PO_TYPE:
+
+        "poType",
+
+    BRAND:
+
+        "brand",
+
+    STYLE:
+
+        "style",
+
+    RETURN_REASON:
+
+        "returnReason",
+
+    TREND:
+
+        "trend"
+
+};
