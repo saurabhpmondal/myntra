d8c69fe9542dabf79ddf9d33f42907441f95242f
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-14 13:52:18 +0530

    Update initializeSalesReturn.js

--- src/pages/salesReturn/services/initializeSalesReturn.js
+++ src/pages/salesReturn/services/initializeSalesReturn.js
@@ -2,28 +2,10 @@
  * =====================================================
  * Project Phoenix
  * Product : Myntra Analytics
- * Module  : Initialize Sales & Return
- * Version : V3.0
+ * Module  : Initialize
+ * Version : V12.0
  * =====================================================
  */
-
-import {
-
-    DataStore
-
-}
-
-from "../../../services/dataService.js";
-
-import {
-
-    resetSalesReturnStore,
-
-    SalesReturnStore
-
-}
-
-from "../store/salesReturnStore.js";
 
 import {
 
@@ -33,113 +15,15 @@ import {
 
 from "../renderers/renderLayout.js";
 
-import {
-
-    loadSalesReturn
-
-}
-
-from "../loadSalesReturn.js";
-
-/**
- * =====================================================
- * Initialize
- * =====================================================
- */
-
 export async function initializeSalesReturn(
 
     container
 
 ){
 
-    /**
-     * ==========================================
-     * Preserve Loaded Data
-     * ==========================================
-     */
-
-    const existingSales=
-
-        SalesReturnStore.salesRows;
-
-    const existingReturns=
-
-        SalesReturnStore.returnRows;
-
-    /**
-     * ==========================================
-     * Reset Store
-     * ==========================================
-     */
-
-    resetSalesReturnStore();
-
-    /**
-     * ==========================================
-     * Restore Data
-     * ==========================================
-     */
-
-    SalesReturnStore.salesRows=
-
-        existingSales.length
-
-        ?
-
-        existingSales
-
-        :
-
-        DataStore.sales;
-
-    SalesReturnStore.returnRows=
-
-        existingReturns.length
-
-        ?
-
-        existingReturns
-
-        :
-
-        DataStore.returns;
-
-    /**
-     * ==========================================
-     * Render Layout
-     * ==========================================
-     */
-
     await renderLayout(
 
         container
-
-    );
-
-    /**
-     * ==========================================
-     * Status
-     * ==========================================
-     */
-
-    SalesReturnStore.loaded=true;
-
-    SalesReturnStore.generatedOn=
-
-        new Date();
-
-    /**
-     * ==========================================
-     * Load Dashboard
-     * ==========================================
-     */
-
-    await loadSalesReturn(
-
-        SalesReturnStore.salesRows,
-
-        SalesReturnStore.returnRows
 
     );
 
