8255a88ecbadf6716e3ea9ed768939f5dcea83b4
saurabhpmondal <saurabhpmondal@gmail.com>
2026-07-15 12:56:07 +0530

    Update refreshDashboard.js

--- src/pages/salesReturn/services/refreshDashboard.js
+++ src/pages/salesReturn/services/refreshDashboard.js
@@ -3,7 +3,7 @@
  * Project Phoenix
  * Product : Myntra Analytics
  * Module  : Refresh Dashboard
- * Version : V12.0
+ * Version : V12.1
  * =====================================================
  */
 
@@ -12,6 +12,8 @@ import { SalesReturnStore } from "../store/salesReturnStore.js";
 import { filterData } from "../engines/filterData.js";
 
 import { buildLookup } from "../engines/buildLookup.js";
+
+import { buildKPIs } from "../engines/buildKPIs.js";
 
 /**
  * =====================================================
@@ -25,7 +27,7 @@ export async function refreshDashboard(){
 
     /**
      * ==========================================
-     * Reset Working Data
+     * Reset
      * ==========================================
      */
 
@@ -34,6 +36,20 @@ export async function refreshDashboard(){
     SalesReturnStore.returns = [];
 
     SalesReturnStore.lookup = {};
+
+    SalesReturnStore.dashboard = {
+
+        sale:{gmv:0,units:0},
+
+        cancel:{gmv:0,units:0},
+
+        rto:{gmv:0,units:0},
+
+        cx:{gmv:0,units:0},
+
+        net:{gmv:0,units:0}
+
+    };
 
     /**
      * ==========================================
@@ -49,12 +65,6 @@ export async function refreshDashboard(){
 
     } = filterData();
 
-    /**
-     * ==========================================
-     * Save Filtered Dataset
-     * ==========================================
-     */
-
     SalesReturnStore.sales = sales;
 
     SalesReturnStore.returns = returns;
@@ -68,6 +78,22 @@ export async function refreshDashboard(){
     SalesReturnStore.lookup = buildLookup(
 
         SalesReturnStore.sales
+
+    );
+
+    /**
+     * ==========================================
+     * Build KPI
+     * ==========================================
+     */
+
+    SalesReturnStore.dashboard = buildKPIs(
+
+        SalesReturnStore.sales,
+
+        SalesReturnStore.returns,
+
+        SalesReturnStore.lookup
 
     );
 
@@ -120,6 +146,50 @@ export async function refreshDashboard(){
         ).length
 
     );
+
+    console.table({
+
+        "Sale GMV":
+
+            SalesReturnStore.dashboard.sale.gmv,
+
+        "Sale Units":
+
+            SalesReturnStore.dashboard.sale.units,
+
+        "Cancel GMV":
+
+            SalesReturnStore.dashboard.cancel.gmv,
+
+        "Cancel Units":
+
+            SalesReturnStore.dashboard.cancel.units,
+
+        "RTO GMV":
+
+            SalesReturnStore.dashboard.rto.gmv,
+
+        "RTO Units":
+
+            SalesReturnStore.dashboard.rto.units,
+
+        "CX GMV":
+
+            SalesReturnStore.dashboard.cx.gmv,
+
+        "CX Units":
+
+            SalesReturnStore.dashboard.cx.units,
+
+        "Net GMV":
+
+            SalesReturnStore.dashboard.net.gmv,
+
+        "Net Units":
+
+            SalesReturnStore.dashboard.net.units
+
+    });
 
     console.groupEnd();
 
